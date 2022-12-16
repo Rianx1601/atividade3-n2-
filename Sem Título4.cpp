b9f57a5245c153ae53697788b4d5a6a9375e55da
Rianx1601 <115299227+Rianx1601@users.noreply.github.com>
2022-12-16 16:45:26 -0300

    Add files via upload

---
+++ Sem Título4.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#difine pi 3,14
+
+float  dob (float w){
+	
+int d;
+
+if(d==1){
+	
+	w=
+}
+if(d==2){
+	
+}else{
+	printf("valor nao definido");
+}	
+	
+	
+	
+	return d;
+}
+
+
+int main() {
+    float x;
+    int y;
+    scanf("%f",&x);
+    printf("celcios 1 ou farenrite 2")
+    
+     scanf("%d",&y);
+    dob(x,y)
+     
+     
+    return 0;
+}
